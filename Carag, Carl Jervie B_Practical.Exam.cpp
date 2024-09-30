2630c15e3afdabc74c6a9eedbb5d375fe014ab82
CarlCarag <qcjbcarag@tip.edu.ph>
2024-09-30 15:16:23 +0800

    Add files via upload

---
+++ Carag, Carl Jervie B_Practical.Exam.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <list>
+using namespace std; 
+
+int main()
+{
+    number{};
+    for (numbers= number);
+    numbers=[5,4,3,2,1];
+    numbers.reverse() << number; 
+    
+    std::cout<<"Original Numbers: " << numbers;
+    std::cout<<"Reversed Numbers: " << number;
+    return 0;
+}
